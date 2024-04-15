777c475ae9ccd5a7314afa14cd5672f45ab74b96
Hugo Emanuel Ladeiro Nobre <hladeiro@c2r7s7.42lisboa.com>
2024-04-15 19:43:31 +0100

    adding lst map

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,54 +6,66 @@
 /*   By: hladeiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 19:24:26 by hladeiro          #+#    #+#             */
-/*   Updated: 2024/04/15 16:42:11 by hladeiro         ###   ########.fr       */
+/*   Updated: 2024/04/15 19:43:10 by hladeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_list	lst_new(void *content)
+static t_list	*lst_new(void *content)
 {
 	t_list	*n_list;
 
 	if (!content)
 		return (NULL);
 	n_list = (t_list *)malloc(sizeof(*n_list));
-	if (!list)
+	if (!n_list)
 		return (NULL);
 	n_list->content = content;
 	n_list->next = NULL;
 	return (n_list);	
 }
 
+static t_list	*ft_lst_last(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
 static void	lstadd_last(t_list **lst, t_list *new)
 {
 	t_list	*new_list;
 
-	new_list = *lst;
+	new_list = ft_lst_last(*lst);
 	if (!new_list)
-		return (NULL);
+		*lst = new;
+	else
+		new_list->next = new;
 
 }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
+	t_list	*list_t;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	if (!new_list)
 	{
-		new_list = ft_lstnew(f(lst->content));
+		new_list = lst_new(f(lst->content));
 		if (!new_list->content)
 			del(new_list->content);
 		lst = lst->next;
 	}
 	while (lst)
 	{
-		lst->content = f(lst->content);
-		ft_lstadd_back(&lst, new_list);
+		list_t = lst_new(f(lst->content));
+		lstadd_last(&new_list, list_t);
 		lst = lst->next;
 	}
 	return (new_list);
