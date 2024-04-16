8548b27a09669551305b11391841f6827aa3a3a4
Hugo Emanuel Ladeiro Nobre <hladeiro@c2r12s1.42lisboa.com>
2024-04-16 14:38:26 +0100

    adding new version of trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,83 +5,70 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hladeiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 18:45:17 by hladeiro          #+#    #+#             */
-/*   Updated: 2024/04/15 19:47:02 by hladeiro         ###   ########.fr       */
+/*   Created: 2024/04/16 14:15:35 by hladeiro          #+#    #+#             */
+/*   Updated: 2024/04/16 14:36:43 by hladeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_letters(const char *s1, const char *s2)
-{
-	char	*ptr;
-
-	char	*p;
-	int		i;
-	int		j;
-	int		result;
-
-	p = (char *) s2;
-	ptr = (char *) s1;
-	i = 0;
-	j = 0;
-	result = 0;
-	while (ptr[i])
-	{
-		while (p[j] != ptr[i] && p[j])
-		{
-			j++;
-		}
-		if (!p[j])
-		{
-			result++;
-			j = 0;
-		}
-		i++;
-	}
-	return (result);
-}
-
-static int	check_str(char s1, const char *set)
+static int	check_set(char *s1, char *set)
 {
 	int	i;
 
 	i = 0;
-	while ((char)set[i] != s1 && set[i])
+	while (*(s1 + i) && *(set + i))
 	{
+		if (s1[i] != set[i])
+			return (0);
 		i++;
 	}
-	if ((char)set[i] == s1)
-		return (0);
 	return (1);
 }
 
-char	*ft_strtrim(const char *s1, const char *set)
+static	int	check_final_set(char *s1, char *set)
+{
+	size_t	s1_len;
+	size_t	set_len;
+
+	s1_len = ft_strlen(s1);
+	set_len = ft_strlen(set);
+
+	while (s1[--s1_len] && set[--set_len])
+	{
+		if (s1[s1_len] != set[set_len])
+			return (0);
+	}
+	return (1);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
-	int		len;
 	int		i;
+	int		end;
 	int		j;
 
 	if (!s1)
 		return (NULL);
-	len = count_letters(s1, set);
-	if (!len)
-		return (ft_strdup(""));
-	str = ft_calloc(len + 1, sizeof(char));
+	if (check_set((char *)s1, (char *)set))
+		i = (int) ft_strlen(set) - 1;
+	else
+		i = 0;
+
+	if (check_final_set((char *) s1, (char *) set))
+		end = (int) (ft_strlen(s1) - ft_strlen(set)) - 1;
+	else
+		end = (int) ft_strlen(s1);
+	str = ft_calloc(end - i, sizeof(char *));
 	if (!str)
 		return (NULL);
-	i = 0;
 	j = 0;
-	while (s1[i])
+	while (i < end)
 	{
-		if (check_str((char)s1[i], set))
-		{
-			str[j] = s1[i];
-			j++;
-		}
+		str[j] = s1[i];
 		i++;
+		j++;
 	}
-	str[j] = '\0';
 	return (str);
 }
